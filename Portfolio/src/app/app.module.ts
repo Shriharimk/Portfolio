0351b0306862147b3fa3c7a93a1b5a1de0580e01
Shrihari <shriharimk01@gamil.com>
2024-03-27 21:02:18 +0530

    Added Material

---
+++ Portfolio/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule
+    // Add other Angular Material modules here as needed
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
